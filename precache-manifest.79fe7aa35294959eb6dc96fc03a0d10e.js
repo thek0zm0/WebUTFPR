2e5ac53b2f512249f7e90c1ed490ae77d7dca587
thek0zm0 <69226581+thek0zm0@users.noreply.github.com>
2020-09-28 13:38:44 -0300

    Updates

---
+++ precache-manifest.79fe7aa35294959eb6dc96fc03a0d10e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b74ee959f5035ab00bfc4d9d1df80816",
+    "url": "/WebUTFPR/index.html"
+  },
+  {
+    "revision": "d847d36b7fe605fe7033",
+    "url": "/WebUTFPR/static/css/main.d60c3af9.chunk.css"
+  },
+  {
+    "revision": "a11607f2a9db324df80e",
+    "url": "/WebUTFPR/static/js/2.6aa934be.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/WebUTFPR/static/js/2.6aa934be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d847d36b7fe605fe7033",
+    "url": "/WebUTFPR/static/js/main.62b97eb7.chunk.js"
+  },
+  {
+    "revision": "7580b9464a1026b87cd3",
+    "url": "/WebUTFPR/static/js/runtime-main.7954d96e.js"
+  }
+]);
