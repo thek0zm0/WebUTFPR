fea2a5e755728e295e74f51140de208f60495fbf
thek0zm0 <69226581+thek0zm0@users.noreply.github.com>
2020-09-30 12:02:49 -0300

    Novo Deploy (verificação email @)

--- build/precache-manifest.403c7f2a7963f020dda54a80a77cacec.js
+++ build/precache-manifest.403c7f2a7963f020dda54a80a77cacec.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6f042b8ee255470493a5e4f1b7b951a1",
+    "revision": "773752d477758e0176d54c633241770a",
     "url": "./index.html"
   },
   {
-    "revision": "b6234a781c56891b2c5f",
+    "revision": "d7b69d48ab05c3de1e24",
     "url": "./static/css/main.5a3a673e.chunk.css"
   },
   {
-    "revision": "53fb2472483bb5da26af",
-    "url": "./static/js/2.b69d321b.chunk.js"
+    "revision": "4d0f0566a835fa37ff1f",
+    "url": "./static/js/2.8a61ecb2.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "./static/js/2.b69d321b.chunk.js.LICENSE.txt"
+    "url": "./static/js/2.8a61ecb2.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "b6234a781c56891b2c5f",
-    "url": "./static/js/main.96b7a0ed.chunk.js"
+    "revision": "d7b69d48ab05c3de1e24",
+    "url": "./static/js/main.4570f64c.chunk.js"
   },
   {
     "revision": "cc313e4fb7f223598d65",
